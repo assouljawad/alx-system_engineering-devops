ec2dd875bd2618e051f5fdb6f9c6a3e0befe13e4
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-10-05 16:21:01 +0100

    all ok

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# change configure nginx
+
+exec { 'update_all':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
+}
+
+-> exec { 'install_nginx':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+-> exec { 'add_content':
+    command  => 'sudo sed -i "/listen 80 default_server;/ a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
+    provider => shell,
+}
+
+-> exec { 'restart_server':
+    command  => 'sudo service nginx restart',
+    provider => shell,
+}
